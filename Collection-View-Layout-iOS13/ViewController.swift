1858a2538366617e8785dc08a24313566a81a0ca
atikhonov <uni.nova.tih@gmail.com>
2020-04-02 20:19:50 +0200

    init commit

---
+++ Collection-View-Layout-iOS13/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Collection-View-Layout-iOS13
+//
+//  Created by atikhonov on 02.04.2020.
+//  Copyright © 2020 atikhonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
