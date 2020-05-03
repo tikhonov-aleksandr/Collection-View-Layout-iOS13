b04d84e46d5ade9a75ec231ce486fce388f2c3db
Aleksandr Tikhonov <uni.nova.tih@gmail.com>
2020-05-03 21:26:13 +0200

    added one more cell

--- Collection-View-Layout-iOS13/Cards/CardsViewController.swift
+++ Collection-View-Layout-iOS13/Cards/CardsViewController.swift
@@ -12,6 +12,8 @@ class CardsViewController: UIViewController {
     
     enum Section: Int {
         case main
+        case info
+        case settings
     }
     
     var dataSource: UICollectionViewDiffableDataSource<Section, Int>! = nil
@@ -26,6 +28,7 @@ class CardsViewController: UIViewController {
     
     private func configureHierarchy() {
         collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
+        collectionView.delegate = self
         collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         collectionView.backgroundColor = .systemBackground
         let nib1 = UINib(nibName: ActivateCollectionViewCell.reuseIdentifier, bundle: nil)
@@ -34,6 +37,12 @@ class CardsViewController: UIViewController {
         collectionView.register(nib2, forCellWithReuseIdentifier: ActionCollectionViewCell.reuseIdentifier)
         let nib3 = UINib(nibName: SubmitCollectionViewCell.reuseIdentifier, bundle: nil)
         collectionView.register(nib3, forCellWithReuseIdentifier: SubmitCollectionViewCell.reuseIdentifier)
+        let nib4 = UINib(nibName: InfoCollectionViewCell.reuseIdentifier, bundle: nil)
+        collectionView.register(nib4, forCellWithReuseIdentifier: InfoCollectionViewCell.reuseIdentifier)
+        
+        let nib5 = UINib(nibName: SettingsCollectionViewCell.reuseIdentifier, bundle: nil)
+        collectionView.register(nib5, forCellWithReuseIdentifier: SettingsCollectionViewCell.reuseIdentifier)
+        
         view.addSubview(collectionView)
     }
     
@@ -53,6 +62,14 @@ class CardsViewController: UIViewController {
                 guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SubmitCollectionViewCell.reuseIdentifier, for: indexPath) as? SubmitCollectionViewCell else { fatalError("Cannot create the cell") }
                // cell.titleLabel.text = "Submit"
                 c = cell
+            } else if identifier == 4 || identifier == 5 {
+                guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: InfoCollectionViewCell.reuseIdentifier, for: indexPath) as? InfoCollectionViewCell else { fatalError("Cannot create the cell") }
+               // cell.titleLabel.text = "Submit"
+                c = cell
+            } else if identifier == 6 {
+                guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SettingsCollectionViewCell.reuseIdentifier, for: indexPath) as? SettingsCollectionViewCell else { fatalError("Cannot create the cell") }
+               // cell.titleLabel.text = "Submit"
+                c = cell
             } else {
                 fatalError()
             }
@@ -63,6 +80,10 @@ class CardsViewController: UIViewController {
         var snapshot = NSDiffableDataSourceSnapshot<Section, Int>()
         snapshot.appendSections([.main])
         snapshot.appendItems(Array(1...3))
+        snapshot.appendSections([.info])
+        snapshot.appendItems(Array(4...5))
+        snapshot.appendSections([.settings])
+        snapshot.appendItems([6])
         dataSource.apply(snapshot, animatingDifferences: false)
     }
 }
@@ -75,7 +96,7 @@ extension CardsViewController {
             return section
         }
         let config = UICollectionViewCompositionalLayoutConfiguration()
-        config.interSectionSpacing = 16.0
+        config.interSectionSpacing = 20.0
         let layout = UICollectionViewCompositionalLayout(sectionProvider: sectionProvider, configuration: config)
         return layout
     }
@@ -84,6 +105,10 @@ extension CardsViewController {
         switch section {
         case .main:
             return mainSection()
+        case .info:
+            return infoSection()
+        case .settings:
+            return settingsSection()
         }
     }
     
@@ -94,16 +119,49 @@ extension CardsViewController {
         let itemSize2 = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(1.0))
         let item2 = NSCollectionLayoutItem(layoutSize: itemSize2)
         
-        let itemSize3 = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(1.0/2.0)) // .fractionalWidth(1.0/2.0)
+        let itemSize3 = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(1.0/2.0))
         let item3 = NSCollectionLayoutItem(layoutSize: itemSize3)
         
         let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(1.0))
         let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item, item2, item3])
-//        group.contentInsets = .init(top: 8.0, leading: 8.0, bottom: 8.0, trailing: 8.0)
-        group.interItemSpacing = .fixed(20)
+        group.interItemSpacing = .fixed(16)
+        
         let section = NSCollectionLayoutSection(group: group)
-        // section.orthogonalScrollingBehavior = .continuous
         return section
     }
     
+    private func infoSection() -> NSCollectionLayoutSection {
+        
+        let itemSize1 = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(1.0/2.0))
+        let item1 = NSCollectionLayoutItem(layoutSize: itemSize1)
+        
+        let itemSize2 = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(1.0/2.0))
+        let item2 = NSCollectionLayoutItem(layoutSize: itemSize2)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(1.0))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item1, item2])
+        group.interItemSpacing = .fixed(4)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        return section
+    }
+     
+    private func settingsSection() -> NSCollectionLayoutSection {
+        
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(1.0))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        return section
+    }
+    
+}
+
+extension CardsViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(#function)
+    }
 }
---
+++ Collection-View-Layout-iOS13/Cards/Cells/SettingsCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsCollectionViewCell.swift
+//  Collection-View-Layout-iOS13
+//
+//  Created by Tikhonov, Aleksandr on 03.05.20.
+//  Copyright © 2020 atikhonov. All rights reserved.
+//
+
+import UIKit
+
+class SettingsCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet var chevronImageView: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    
+    static let reuseIdentifier = "SettingsCollectionViewCell"
+
+}
